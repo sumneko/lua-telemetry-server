f9ad1bf49efdc49626ea94406aa7eba926bb1a22
最萌小汐 <sumneko@hotmail.com>
2020-12-14 19:10:33 +0800

    pcall

--- main.lua
+++ main.lua
@@ -8,10 +8,14 @@ local methods = {
     ['pulse'] = require 'method.pulse'
 }
 
+local function pushMethod(data)
+    local method, token, index = string.unpack('zz', data)
+    methods[method](token, data:sub(index))
+end
+
 function link:on_accept(stream)
     function stream:on_data(data)
-        local method, token, index = string.unpack('zz', data)
-        methods[method](token, data:sub(index))
+        xpcall(pushMethod, print, data)
     end
     stream:update()
 end
