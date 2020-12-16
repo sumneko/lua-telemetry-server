9d4182b6114642f297931a6a25a929aad4ca9395
最萌小汐 <sumneko@hotmail.com>
2020-12-16 13:19:46 +0800

    过滤掉重复的错误信息

--- method/error.lua
+++ method/error.lua
@@ -5,8 +5,14 @@ local util  = require 'utility'
 
 log.init(fs.path '', fs.path 'log/error.log')
 
+local caches = {}
+
 return function (token, stream)
     local client, errq = string.unpack('zz', stream)
     local err = assert(load('return ' .. errq))()
+    if caches[err] then
+        return
+    end
+    caches[err] = true
     log.info(('client: %s\n%s'):format(client, err))
 end
